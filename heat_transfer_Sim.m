26a0c882267ee69f09441a288c7b81b389a53b73
willharr <36304427+willharr@users.noreply.github.com>
2019-11-18 17:39:57 +0000

    Added nozzle transfer equation

--- heat_transfer_Sim.m
+++ heat_transfer_Sim.m
@@ -2,17 +2,21 @@
 % Will Harradence
 % Imperial Aeronautics 2019/20
 
+%% Notes
+% *_i denotes a value in Imperial units, generally BTU/ft/lbm unless
+% otherwise specified. All other variables use SI units (J/m/kg etc.)
+
 clear 
 clc
 close all
 
 %% Grid Setup
 
-L = 0.1;
-W = 0.05;
+L = 1; %m
+W = 1; %m
 
-N = 100;
-M = 100;
+N = 20;
+M = 22;
 
 dx = L/(N);
 dy = W/(M);
@@ -25,8 +29,10 @@ x = repmat(x1,M,1); %list of x-pos for each y-pos
 y = repmat(y1,1,N); %list of y-pos for each x-pos
 
 %temp array
-u = 300*ones(M,N);
-du = u;
+temp_init = 500;
+
+u = temp_init*ones(M,N);
+du = u.*0;
 
 
 %% Material Properties
@@ -63,54 +69,91 @@ Cp_g_i = 4.125e-1; %BTU/lb*R
 C_w = 376; %J/kg*K 
 k = 413; %conduction constant
 
+%Nitrous properties
+C_nos = 72; %J/mol*K
+molar_nos = 0.044; %kg/mol
+C_nos = C_nos/0.044; %J/kg*K
+
 %% Timestep
 tfin = 1;
 
-dtmax = 0.25*dx*dy/k; 
+dtmax = 0.25*dx*dy/k;
+disp(dtmax)
 dt = input('Input timestep: ');
 %dt = dtmax/2;
 
-C = k*dt/(dx*dy);
+const = k*dt/(dx*dy);
 
 %% Intial Conditions
 
-u(1,:) = 400;
-u(M,:) = 400;
+u(1,:) = temp_init;
+u(M,:) = temp_init;
 
 %% Calculation Loop
 t = 0;
-
 while t<tfin
     
     t = t+dt
     
-    
-    T_aw = T_ns_i*((1+r*((gamma-1)/2)*(M^2))/(1+((gamma-1)/2)*(M^2)));
-    sigma = (((0.5*(u(1,N/2)/T_ns_i)*(1+((gamma-1)/2)*(mach^2))+0.5)^(0.68))*(1+((gamma-1)/2)*mach^2)^0.12)^(-1);
-    h_i = ((0.026/Dia_i^(0.2))*(((mu_i^0.2)*Cp_g_i)/Pr^0.6)*((P_throat_i/c_star_i)^0.8)*(Dia_i/r_throat)^0.1)*(4.5^0.9)*sigma;
-    
-    h = h_i/2.044e4;
-    q = h*(T_aw-u(1,N/2));
-    u(1,:) = q*C_w*W^2;
-    disp(u(1,1))
-    
+    % Conduction through wall
     for i = 2:M-1
         for j = 2:N-1
             
-            du(i,j) = C*(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j-1)-4*u(i,j));
+            du(i,j) = const*(u(i+1,j)+u(i-1,j)+u(i,j+1)+u(i,j-1)-4*u(i,j));
             
         end
     end
     
-    u = u + du;
+    du(1,:) = du(2,:);
+    du(M,:) = du(M-1,:);
+    
+    % Nozzle Side Heat Transfer
+    T_aw_g = T_ns*((1+r*((gamma-1)/2)*(mach^2))/(1+((gamma-1)/2)*(mach^2)));
+    
+    %the following 2 lines are in imperial units (due to empirical
+    %constants)
+    sigma_i = (((0.5*(u(1,N/2)/T_ns_i)*(1+((gamma-1)/2)*(mach^2))+0.5)^(0.68))*(1+((gamma-1)/2)*mach^2)^0.12)^(-1);
+    h_g_i = ((0.026/Dia_i^(0.2))*(((mu_i^0.2)*Cp_g_i)/Pr^0.6)*((P_throat_i/c_star_i)^0.8)*(Dia_i/r_throat)^0.1)*(4.5^0.9)*sigma_i;
+    
+    h_g = h_g_i/2.044e4; %convert heat transfer coeff to SI
+    q_g = h_g*(T_aw_g-u(1,1));
+    Q_g = q_g*C_w*dx;
+    
+    du(1,:) = du(1,:) + Q_g ; %set nozzle side temp change w.r.t. heat transfer
+    
+    % Coolant side heat transfer
+    T_aw_c = 300;
+    h_c = (2.15e-8)*5;
+    
+    q_c = h_c*(T_aw_c-u(M,N/2));
+    Q_c = q_c*C_nos*dx;
+    
+    du(M,:) = du(M,:) + Q_c;
 
+    
+    %Update temp matrix
+    u = u + du;
+    
+    %Enforce boundary conditions
     %u(1,:) = 2500;
-    u(M,:) = 200;
+    %u(M,:) = temp_init;
     
     u(:,1) = u(:,2);
     u(:,N) = u(:,N-1);
     
+    %plot
+    figure(1)
     surf(x,y,u)
+%     figure(2)
+%     subplot(3,1,1)
+%     bar([0,1],[du(1,1),du(M,1)])
+%     xlabel('du')
+%     subplot(3,1,2)
+%     bar([0,1],[q_g,q_c])
+%     xlabel('q')
+%     subplot(3,1,3)
+%     bar([0,1],[u(1,1),u(M,1)])
+%     xlabel('u')
     drawnow
 
 end
